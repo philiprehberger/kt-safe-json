ec0bf78dad4cc7fe8bd6982bdf5268700ce9ed19
Reverend Philip <me@philiprehberger.com>
2017-08-25 18:35:01 -0600

    Release v0.1.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     kotlin("jvm") version "2.0.21"
-    kotlin("plugin.serialization") version "1.9.22"
+    kotlin("plugin.serialization") version "2.0.21"
     `maven-publish`
     signing
 }
